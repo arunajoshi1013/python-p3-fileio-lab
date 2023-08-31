f4c6716fba2c9069273ad6266119397031347f19
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-08-30 19:07:33 -0700

    file io lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    fname = str(file_name) + ".txt"
+    with open(fname, 'w', encoding='utf-8') as f:
+        f.write(file_content)
+    return
 
 def append_file(file_name, append_content):
-    pass
+    fname = str(file_name) + ".txt"
+    with open(fname, 'a', encoding='utf-8') as f:
+        f.write(append_content)
+    return
 
 def read_file(file_name):
-    pass
+    fname = str(file_name) + ".txt"
+    with open(fname, 'r', encoding='utf-8') as f:
+        return f.read()
